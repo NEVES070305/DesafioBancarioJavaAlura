153ccc80a7bf5a10d23428f4a12a2def0df3178a
Guilherme Neves da Silva <guilhermenevesdasilva321@gmail.com>
2023-08-17 20:29:40 -0300

    Mudanças e opinião do autor

--- DesafioBancarioJava.java
+++ DesafioBancarioJava.java
@@ -34,9 +34,10 @@ public class DesafioBancarioJava {
                 4- Sair
                                 
                 Digite a opção desejada:""";
+        System.out.println(extrato);
 
         while(processo){
-        System.out.println(extrato);
+
         System.out.println(operacoes);
         operacaoEscolhida = leitura.nextInt();
 
@@ -51,14 +52,72 @@ public class DesafioBancarioJava {
                 break;
             case 3:
                 System.out.println("Quando deseja transferir: ");
-                saldo-=leitura.nextDouble();
-                System.out.println("Seu salado passsa a ser: "+ saldo);
+                if (saldo>=leitura.nextDouble()) {
+                    saldo -= leitura.nextDouble();
+                    System.out.println("Seu salado passsa a ser: " + saldo);
+                }else{
+                    System.out.println("Não há saldo suficiente.");
+                }
                 break;
             case 4:
                 System.out.println("Processo finalizado.");
                 processo = false;
                 break;
-
+            default:
+                System.out.println("Opção inválida.");
+                break;
         }}
     }
 }
+
+/*import java.util.Scanner;
+
+public class Desafio {
+    public static void main(String[] args) {
+        String nome = "Clark Kent";
+        String tipoConta = "Corrente";
+        double saldo = 1599.99;
+        int opcao = 0;
+
+        System.out.println("***********************");
+        System.out.println("\nNome do cliente: " + nome);
+        System.out.println("Tipo conta: " + tipoConta);
+        System.out.println("Saldo atual: " + saldo);
+        System.out.println("\n***********************");
+
+        String menu = """
+                ** Digite sua opção **
+                1 - Consultar saldo
+                2 - Transferir valor
+                3 - Receber valor
+                4 - Sair
+
+                """;
+        Scanner leitura = new Scanner(System.in);
+
+        while (opcao != 4) {
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+
+            if (opcao == 1){
+                System.out.println("O saldo atualizado é " + saldo);
+            } else if (opcao == 2) {
+                System.out.println("Qual o valor que deseja transferir?");
+                double valor = leitura.nextDouble();
+                if (valor > saldo) {
+                    System.out.println("Não há saldo para realizar a transferência.");
+                } else {
+                    saldo -= valor;
+                    System.out.println("Novo saldo: " + saldo);
+                }
+            } else if (opcao == 3) {
+                System.out.println("Valor recebido: ");
+                double valor = leitura.nextDouble();
+                saldo += valor;
+                System.out.println("Novo saldo: " + saldo);
+            } else if (opcao != 4) {
+                System.out.println("Opção inválida!");
+            }
+        }
+    }
+}*/
